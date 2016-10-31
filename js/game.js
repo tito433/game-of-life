a835809e93446afecd4fc1a2a9e81006f4e2419a
tito <tito@cefalo.com>
2016-10-31 12:36:41 +0600

    random init function added

--- js/game.js
+++ js/game.js
@@ -7,10 +7,7 @@ function GameOfLife(canvas){
     this.width=canvas.width;
     this.height=canvas.height;
 	this._ctx=canvas.getContext("2d");
-	this.cells=new Array(Math.floor(this.height/size));
-	for (var i = 0; i < this.cells.length; i++) {
-	  this.cells[i] = new Array(Math.floor(this.width/size));
-	}
+	
 
 	this.draw=function(){
 		this._ctx.clearRect(0,0,this.width,this.height);
@@ -20,8 +17,36 @@ function GameOfLife(canvas){
 				var dx=x*size,dy=y*size;
 				this._ctx.beginPath();;
 				this._ctx.rect(dx,dy,size,size);
-				this._ctx.stroke();
+				if(this.cells[y][x]===true){
+					this._ctx.fill();
+				}else{
+					this._ctx.stroke();
+				}
+				
 			}
 		}
 	}
+
+	this._createCells=function(){
+		this.cells=new Array(Math.floor(this.height/size));
+		for (var i = 0; i < this.cells.length; i++) {
+		  this.cells[i] = new Array(Math.floor(this.width/size));
+		}
+	}
+	this.randomize=function(){
+		this._createCells();
+
+		var initLife=20,
+			my=this.cells.length,
+			mx=this.cells[0].length;
+
+		while(initLife--){
+			var x=Math.floor(Math.random()*mx),
+				y=Math.floor(Math.random()*my);
+			if(this.cells[x] !=undefined)
+				this.cells[x][y]=true;	
+		}
+
+	}
+	this._createCells();
 }
