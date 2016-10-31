1ab560a8dbb6bad76841e0382d4795fed1386a53
tito <tito@cefalo.com>
2016-10-31 14:19:37 +0600

    mouse click added

--- js/game.js
+++ js/game.js
@@ -3,15 +3,16 @@ function GameOfLife(canvas){
 	
 	canvas.width=parent.clientWidth;
     canvas.height=parent.clientHeight;
+
     var size=10;
+    this.bounds=canvas.getBoundingClientRect();
     this.width=canvas.width;
     this.height=canvas.height;
 	this._ctx=canvas.getContext("2d");
-	
+	this.speed=500;
 
 	this.draw=function(){
 		this._ctx.clearRect(0,0,this.width,this.height);
-
 		for (var y = 0,ny=this.cells.length; y < ny; y++) {
 			for(var x=0,nx=this.cells[y].length;x<nx;x++){
 				var dx=x*size,dy=y*size;
@@ -25,6 +26,37 @@ function GameOfLife(canvas){
 				
 			}
 		}
+	}
+	this.run=function(){
+		var deltas =[
+				{x:-1, y:-1}, {x:0, y:-1}, {x:1, y:-1},
+               {x:-1, y:0},               {x:1, y:0},
+               {x:-1, y:1},  {x:0, y:1},  {x:1, y:1}];
+
+		for (var y = 0,ny=this.cells.length; y < ny; y++) {
+			for(var x=0,nx=this.cells[y].length;x<nx;x++){
+				var neighbours=[];
+				for(var di in deltas){
+					var delta=deltas[di];
+				    if (x+delta.x < 0 || x + delta.x >= nx ||
+				        y+delta.y < 0 || y + delta.y >= ny)
+				        continue;
+
+				    neighbours.push(this.cells[x + delta.x][y + delta.y]);
+				}
+				
+				var nl=neighbours.map((i)=>i?1:0).reduce((a,b)=>a+b,0);
+				var life=this.cells[x,y]||false;
+				if(life && nl<2) life=false;
+				else if(life && nl>3) life=false;
+				else if(!life && nl==3) life=true;
+
+				this.cells[x][y]=life;
+
+			}
+		}
+
+		this.draw();
 	}
 
 	this._createCells=function(){
@@ -48,5 +80,25 @@ function GameOfLife(canvas){
 		}
 
 	}
+
+	this.start=function(){
+		this._timer=setInterval(this.run.bind(this),this.speed);
+	}
+	this.stop=function(){
+		clearInterval(this._timer);
+	}
+
+	this._onClick=function(evt){
+		var x=evt.clientX - this.bounds.left,
+            y=evt.clientY - this.bounds.top,
+        	xt=Math.floor(x/size),
+        	yt=Math.floor(y/size);
+        if(this.cells[xt]){
+        	this.cells[yt][xt]=!this.cells[yt][xt];
+        }
+        this.draw();
+	}
+
 	this._createCells();
+	canvas.addEventListener("mouseup", this._onClick.bind(this), false);
 }
